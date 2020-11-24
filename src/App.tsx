a3041b73127f66a95af43f293f6174f2a92e7e61
Gu <936285702@qq.com>
2020-11-24 23:17:35 +0800

    添加button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
+import Button,{ButtonType,ButtonSize} from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>1</h1>
-        <h2>2</h2>
-        <h3>3</h3>
-        <h4>4</h4>
-        <h5>5</h5>
-        <code>
-          const a = 'b'
-        </code>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button size={ButtonSize.Small} autoFocus className="my-test">Hello</Button>
+        <Button size={ButtonSize.Small} btnType={ButtonType.Primary} onClick={(e) => {e.preventDefault(); alert(111)}}>Hello</Button>
+        <Button size={ButtonSize.Small} btnType={ButtonType.Danger} >Hello</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>BaiDu Link</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com" size={ButtonSize.large }>BaiDu Link</Button>
+        <br/>
+        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary}>Hello</Button>
+        <Button btnType={ButtonType.Danger}>Hello</Button>
+        <br/>
+        <Button size={ButtonSize.large} disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.large}>Hello Hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.large}>Hello</Button>
+        <br/>
+       
       </header>
     </div>
   );
