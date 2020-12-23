0d5e1b551b00c331c36c86a96dc7463323f1d283
Gu <936285702@qq.com>
2020-12-23 22:55:59 +0800

    添加上传文件列表

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,36 +1,39 @@
 import React  from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import Upload from './upload'
+import Upload,{UploadFile} from './upload'
 //import Button from '../Button/button'
 // import Icon from '../Icon/icon'
-// const defaultFileList: UploadFile[] = [
-//   { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
-//   { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
-//   { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percent: 30 }
-// ]
-const checkFileSize = (file: File) => {
-  if (Math.round(file.size / 1024) > 50) {
-    alert('file too big')
-    return false;
-  }
-  return true;
-}
+const defaultFileList: UploadFile[] = [
+  { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
+  { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
+  { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percent: 30 }
+]
+// const checkFileSize = (file: File) => {
+//   if (Math.round(file.size / 1024) > 50) {
+//     alert('file too big')
+//     return false;
+//   }
+//   return true;
+// }
 const filePromise = (file: File) => {
   const newFile = new File([file], 'new_name.docx', {type: file.type})
   return Promise.resolve(newFile)
 }
 const SimpleUpload = () => {
   return (
-    <Upload
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      onProgress={action('pogress')}
-      onSuccess={action('success')}
-      onError={action('error')}
-      onchange={action('changed')}
-      beforeUpload={filePromise}
-    >
-    </Upload>
+    <div style={{width: 500}}>
+        <Upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          onProgress={action('pogress')}
+          onSuccess={action('success')}
+          onError={action('error')}
+          onchange={action('changed')}
+          beforeUpload={filePromise}
+          defaultFileList={defaultFileList}
+        >
+        </Upload>
+    </div>
   )
 }
 
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,33 +1,70 @@
-import React, { ChangeEvent, FC, useRef } from 'react'
+import React, { ChangeEvent, FC, useRef, useState } from 'react'
 import axios from 'axios'
-
 import Button,{ButtonType} from '../Button/button'
+import UploadList from './uploadList'
+
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
+
+export interface UploadFile {
+  uid: string;
+  size: number;
+  name: string;
+  status?: UploadFileStatus;
+  percent?: number;
+  raw?: File;
+  response?: any;
+  error?: any;
+}
 
 export interface UploadProps {
   action: string;
+  defaultFileList?: UploadFile[];
   beforeUpload?: (file: File) => boolean | Promise<File>;
   onProgress?: (percentage: number, file: File) => void;
   onSuccess?: (data: any, file: File) => void;
   onError?: (err:any, file: File) => void;
   onchange?: (file: File) => void;
+  onRemove?: (file: UploadFile) => void;
 }
 
 const Upload: FC<UploadProps> = (props) => {
   const {
     action,
+    defaultFileList,
     beforeUpload,
     onProgress,
     onSuccess,
     onError,
-    onchange
+    onchange,
+    onRemove,
   } = props
-
+  const fileInput = useRef<HTMLInputElement>(null)
+  const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList ||[])
+  console.log(fileList)
+  const updateFileList = (uploadFiles: UploadFile, uploadObj : Partial<UploadFile>) => {
+      setFileList((prevList) => {
+        return prevList.map((file) => {
+          if(file.uid === uploadFiles.uid){
+            return {...file,...uploadObj}
+          }else{
+            return file
+          }
+        })
+      })
+  }
   const handleClick = ()=> {
     if(fileInput.current){
       fileInput.current.click()
     }
   }
-
+  const handleRemove = (file: UploadFile) => {
+    setFileList((prevList) => {
+      return prevList.filter(item => item.uid !== file.uid)
+    })
+    if(onRemove){
+      onRemove(file)
+    }
+  }
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
     if(!files){
@@ -56,6 +93,16 @@ const Upload: FC<UploadProps> = (props) => {
     })
   }
   const post = (file: File) => {
+    let _file: UploadFile = {
+      uid: Date.now() + 'upload-file',
+      status: 'ready',
+      name: file.name,
+      size: file.size,
+      percent: 0,
+      raw: file
+    }
+
+    setFileList([_file,...fileList])
     const formData = new FormData()
     formData.append(file.name,file)
     axios.post(action,formData, {
@@ -65,13 +112,15 @@ const Upload: FC<UploadProps> = (props) => {
       onUploadProgress: (e) => {
         let percentage = Math.round((e.loaded * 100)/ e.total) || 0
         if(percentage < 100){
+          updateFileList(_file, {percent: percentage, status: 'uploading'})
           if(onProgress){
             onProgress(percentage, file)
           }
         }
       }
     }).then(resp => {
-      console.log(resp)
+      // console.log(resp)
+      updateFileList(_file, {status: 'success',response: resp.data})
       if(onSuccess){
         onSuccess(resp.data,file)
       }
@@ -79,7 +128,8 @@ const Upload: FC<UploadProps> = (props) => {
         onchange(file)
       }
     }).catch(err => {
-      console.error(err)
+      // console.error(err)
+      updateFileList(_file,{status: 'error', error: err})
       if(onError){
         onError(err, file)
       }
@@ -88,7 +138,6 @@ const Upload: FC<UploadProps> = (props) => {
       }
     })
   }
-  const fileInput = useRef<HTMLInputElement>(null)
   return (
     <div className="stepup-upload-component">
       <Button 
@@ -102,6 +151,7 @@ const Upload: FC<UploadProps> = (props) => {
         onChange={handleFileChange}
         style={{display: 'none'}}
         />
+      <UploadList fileList={fileList} onRemove={handleRemove} />
     </div>
   )
 }
