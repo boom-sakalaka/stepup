0b7bdee72ec591a2c97a98ca4f66af2135a104c1
Gu <936285702@qq.com>
2020-12-29 20:31:44 +0800

    新增 button的storybook文档

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import classNames from 'classnames'
+import { type } from 'os'
 
-export enum ButtonSize {
-  large = 'lg',
-  Small = 'sm',
-}
+// export enum ButtonSize {
+//   large = 'lg',
+//   Small = 'sm',
+// }
 
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link',
-}
+// export enum ButtonType {
+//   Primary = 'primary',
+//   Default = 'default',
+//   Danger = 'danger',
+//   Link = 'link',
+// }
+
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+export type ButtonSize = 'lg' | 'sm'
 
 export interface BaseButtonProps {
   className?: string;
@@ -40,9 +44,9 @@ const Button: React.FC<ButtonProps> = (props) => {
   const classes = classNames('btn', className ,{
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    'disabled': (btnType === ButtonType.Link) && disabled
+    'disabled': (btnType === 'link') && disabled
   })
-  if(btnType === ButtonType.Link && href){
+  if(btnType === 'link' && href){
     return (
       <a
         className={classes}
@@ -68,7 +72,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default
+  btnType: 'default',
 }
 
 export default Button
